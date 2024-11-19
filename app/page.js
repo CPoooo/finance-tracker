e04b9e4cd91a8f64c025d1fa5b700b4c1b7ab2e4
cameronpool <cameronpool2019@gmail.com>
2024-11-19 13:30:15 -0600

    nav bar half done, i love tailwind

--- app/page.js
+++ app/page.js
@@ -1,5 +1,23 @@
+import { IoIosStats } from "react-icons/io";
+
 export default function Home() {
   return (
-    <h1 className="text-2xl font-bold">Hello World</h1>
-  )
+    <header className="flex items-center justify-between">
+      <div className="flex items-center gap-2">
+        <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
+          <img
+          className="object-cover w-full h-full"
+            src="https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg"
+            alt="Profile Image"
+          />
+        </div>
+        <small>Hi, Cameron</small>
+      </div>
+
+      <nav className="flex items-center gap-2">
+        <div><IoIosStats /></div>
+        <div>Logout Button</div>
+      </nav>
+    </header>
+  );
 }
